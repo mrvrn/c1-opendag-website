d21a3795abbf234fb9be4d602c4a601d4c0c0880
mrvrn <64770873+mrvrn@users.noreply.github.com>
2022-09-27 10:56:37 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,130 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <meta property="og:title" content="">
+  <meta property="og:type" content="">
+  <meta property="og:url" content="">
+  <meta property="og:image" content="">
+
+  <link rel="manifest" href="site.webmanifest">
+  <link rel="apple-touch-icon" href="icon.png">
+  <!-- Place favicon.ico in the root directory -->
+
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/main.css">
+
+  <meta name="theme-color" content="#fafafa">
+</head>
+
+<body>
+
+  <!-- Add your site or application content here -->
+
+  <header>
+    <div class="container">
+        <nav>
+            <div class="navbar">
+                <div class="logo">
+                    <img src="Logos/curio-16-midden-groen-logo-rgb.png" alt="">
+                </div>
+                <a href="">Home</a>  
+            </div>                        
+        </nav>
+    </div>
+  </header>
+  <main>
+    <div class="container">
+        <h2 class="blokkenh2">Indeling van de blokken</h2>
+        <div class="blokken">
+            <img src="img/opleidingsoverzicht3.png" alt="">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem, molestias? Temporibus accusamus tempore, nostrum suscipit cumque nihil, vel quidem cum cupiditate ratione quibusdam quasi adipisci asperiores voluptates nam reprehenderit velit? Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum voluptates pariatur, sapiente voluptas officiis laboriosam voluptatem quam eaque nulla in ab a animi porro, aspernatur fugiat quia consequuntur quos sint? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus, expedita ipsam culpa quasi saepe vitae eligendi voluptatem veniam quibusdam aliquam nemo pariatur qui illum corrupti excepturi? Voluptatibus commodi ex quidem!</p>
+        </div>
+        <h2 class="roosterh2">Rooster eerstejaars</h2>
+        <div class="rooster">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem, molestias? Temporibus accusamus tempore, nostrum suscipit cumque nihil, vel quidem cum cupiditate ratione quibusdam quasi adipisci asperiores voluptates nam reprehenderit velit? Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum voluptates pariatur, sapiente voluptas officiis laboriosam voluptatem quam eaque nulla in ab a animi porro, aspernatur fugiat quia consequuntur quos sint? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus, expedita ipsam culpa quasi saepe vitae eligendi voluptatem veniam quibusdam aliquam nemo pariatur qui illum corrupti excepturi? Voluptatibus commodi ex quidem!</p>
+            <img src="img/rooster1a.jpg" alt="">
+        </div>
+        <h2 class="vakkenh2">Vakken eerstejaars</h2>
+        <div class="vakken">
+            <div class="vak">
+                <h3>NATIVE</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, alias ratione similique consequatur aspernatur repellendus ipsa dolores quasi ab commodi error aut numquam tempora ipsum eveniet dignissimos molestiae dolor nemo.</p>
+            </div>
+            <div class="vak">
+                <h3>WEB</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae esse eaque consequatur minima totam illum amet provident ex quasi reprehenderit aut, architecto, eum maxime et tempora autem aperiam sint. Eius.</p>
+            </div>
+            <div class="vak">
+                <h3>PRA</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum ad autem beatae pariatur animi similique veniam! Aperiam repudiandae cum quia ea. Excepturi quam fugiat atque reprehenderit corporis labore, sunt numquam.</p>
+            </div>
+            <div class="vak">
+                <h3>PRO</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum, pariatur! Velit repellat voluptates illum placeat voluptatibus repudiandae nihil laborum, molestias commodi iste eum et architecto neque ad! Qui, fuga libero.</p>
+            </div>
+            <div class="vak">
+                <h3>Eng</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error dicta, iusto quidem in nulla tenetur numquam ratione expedita eaque temporibus quis aperiam, consequatur, excepturi eveniet obcaecati explicabo. Id, soluta a.</p>
+            </div>
+            <div class="vak">
+                <h3>Ned</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, architecto sit totam iste nisi voluptatibus laudantium corrupti officia maxime beatae reprehenderit quo ea placeat vero quibusdam suscipit modi ab nulla.</p>
+            </div>
+            <div class="vak">
+                <h3>Rekenen</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod esse est, doloremque doloribus deserunt culpa accusamus eos amet vel dolorem eum saepe optio, natus tempore similique delectus commodi voluptate voluptas?</p>
+            </div>
+            <div class="vak">
+                <h3>Studieloopbaanbegeleiding</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius exercitationem, ex laudantium et id doloribus illo quod temporibus vero unde nobis est saepe non laboriosam? Explicabo odio corporis deserunt. Impedit.</p>
+            </div>
+            <div class="vak">
+                <h3>Por</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti dolore, quod minima, cupiditate totam ad sed ducimus illum, pariatur voluptatem ex atque eveniet. Sunt pariatur est suscipit vel consectetur unde.</p>
+            </div>
+        </div>
+        <div class="versnellen">
+            <h2 class="versnellenh2">Mogelijkheid om te versnellen</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero ad est debitis fuga possimus, qui tempore quis! Mollitia eum veniam, atque eius, laborum accusantium ipsum voluptate voluptatibus soluta necessitatibus quis! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis quis quia odit veniam neque id fugit provident doloremque, ipsam repellendus necessitatibus eius amet animi. Quibusdam est dicta officia facere in.</p>
+        </div>
+        <h2 class="smoelenh2">De leraren</h2>
+        <div class="smoelenboekje">
+            <div class="smoel">
+                <img src="img/leraar1.jpg" alt="">
+                <h3>Michel Meeuwesen</h3>
+            </div>
+            <div class="smoel">
+                <img src="img/leraar2.jpg" alt="">
+                <h3>Ine Vermeer</h3>
+            </div>
+            <div class="smoel">
+                <img src="img/leraar3.jpg" alt="">
+                <h3>Elton Boekhoudt</h3>
+            </div>
+            <div class="smoel">
+                <img src="img/leraar4.jpg" alt="">
+                <h3>Bart Roos</h3>
+            </div>
+        </div>
+    </div>       
+  </main>
+
+  <script src="js/vendor/modernizr-3.11.2.min.js"></script>
+  <script src="js/plugins.js"></script>
+  <script src="js/main.js"></script>
+
+  <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+  <script>
+    window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
+    ga('create', 'UA-XXXXX-Y', 'auto'); ga('set', 'anonymizeIp', true); ga('set', 'transport', 'beacon'); ga('send', 'pageview')
+  </script>
+  <script src="https://www.google-analytics.com/analytics.js" async></script>
+</body>
+
+</html>
